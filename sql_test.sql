b26eb8ddb04aabc387102a72a47d014cb834ba46
Claudio Bo <claudiobo1122@gmail.com>
2022-01-27 23:55:56 -0700

    More scoreboard shit srsly i dont remember what i did

---
+++ sql_test.sql
@@ -0,0 +1,28 @@
+INSERT INTO "main"."Zandronum" VALUES 
+('scores', 'kledioz', '6000', '1643349638.34298'),
+('name', 'kledioz', '[w8]Kledioz-', '1643349638.37199'),
+('scores', 'pitudo', '5000', '1643349638.34298'),
+('name', 'pitudo', '[w7]pitudo-', '1643349638.37199'),
+('scores', 'ojete', '1412', '1643349638.34298'),
+('name', 'ojete', '[w6]ojete-', '1643349638.37199'),
+('scores', 'samuel', '5124', '1643349638.34298'),
+('name', 'samuel', '[w5]samuel-', '1643349638.37199'),
+('scores', 'dark', '1313', '1643349638.34298'),
+('name', 'dark', '[w4]dark-', '1643349638.37199'),
+('scores', 'sexo', '111', '1643349638.34298'),
+('name', 'sexo', '[w3]sexo-', '1643349638.37199'),
+('scores', 'falopa', '1', '1643349638.34298'),
+('name', 'falopa', '[w2]falopa-', '1643349638.37199'),
+('scores', 'logitech', '3331', '1643349638.34298'),
+('name', 'logitech', '[w1]logitech-', '1643349638.37199'),
+('scores', 'predator', '4441', '1643349638.34298'),
+('name', 'predator', '[h8]predator-', '1643349638.37199'),
+('scores', 'gucci', '6666', '1643349638.34298'),
+('name', 'gucci', '[h7]gucci-', '1643349638.37199'),
+('scores', 'nodeberiasalir', '1', '1643349638.34298'),
+('name', 'nodeberiasalir', '[h7]nodeberiasalir-', '1643349638.37199'),
+('scores', 'yotampoco', '2', '1643349638.34298'),
+('name', 'yotampoco', '[h7]yotampoco-', '1643349638.37199'),
+
+
+;
